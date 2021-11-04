5541b015476552729f6b6690023690a80b5f8413
daichi9130 <koumedai03@gmail.com>
2021-11-04 18:16:04 +0000

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,22 @@ class User < ApplicationRecord
 
   validates :name, presence: true,length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50 }
+
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :follower, source: :followed
+  has_many :followers, through: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   get 'book_comments/create'
   get 'book_comments/destroy'
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
