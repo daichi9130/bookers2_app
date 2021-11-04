a2203359dd1743af57a41bb86390ef2595993de4
daichi9130 <koumedai03@gmail.com>
2021-11-04 10:58:57 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about'
-  resources :books
   resources :users
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
+  end
+
 end
